47409819132e5b37af41932ab810d4ffe1e146b7
boringbyte <kottapallisivaram@gmail.com>
2022-08-08 15:32:58 -0400

    Grinding leetcode finally

--- LC/Meta/Revision1/Problems-151-165.py
+++ LC/Meta/Revision1/Problems-151-165.py
@@ -5,6 +5,33 @@ from functools import lru_cache
 from LC.LCMetaPractice import ListNode, TreeNode
 
 
+def odd_even_linked_list(head):
+    if not head or not head.next:
+        return head
+    odd, even, even_head = head, head.next, head.next
+
+    while even and even.next:
+        odd.next = even.next
+        odd = odd.next
+        even.next = odd.next
+        even = even.next
+    odd.next = even_head
+    return head
+
+
+def flatten_binary_tree_to_linked_list(root):
+    current = root
+    while current:
+        if current.left is not None:
+            p = current.left
+            while p.right is not None:
+                p = p.right
+            p.right = current.right
+            current.right = current.left
+            current.left = None
+        current = current.right
+
+
 def toeplitz_matrix(matrix):
     m, n = len(matrix), len(matrix[0])
     for i in range(m - 1):
