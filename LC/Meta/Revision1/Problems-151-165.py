ac00bf6dde7617511800783d8fe80f20862ef44a
boringbyte <kottapallisivaram@gmail.com>
2022-08-08 15:50:31 -0400

    Grinding leetcode finally

--- LC/Meta/Revision1/Problems-151-165.py
+++ LC/Meta/Revision1/Problems-151-165.py
@@ -5,6 +5,53 @@ from functools import lru_cache
 from LC.LCMetaPractice import ListNode, TreeNode
 
 
+def validate_binary_tree_nodes(n, left_child, right_child):
+    root, child_nodes = 0, set(left_child + right_child)
+    for i in range(n):
+        if i not in child_nodes:
+            root = i
+
+    queue, visited = collections.deque([root]), set()
+    while queue:
+        current = queue.popleft()
+        if current in visited:
+            return False
+        visited.add(current)
+        if left_child[current] != -1:
+            queue.append(left_child[current])
+        if right_child[current] != -1:
+            queue.append(right_child[current])
+    return len(visited) == n
+
+
+def largest_bst_subtree(root):
+    pass
+
+
+def number_of_connected_components_in_an_undirected_graph(n, edges):
+    graph = collections.defaultdict(list)
+    result, visited = 0, set()
+
+    def build_graph(u, v):
+        graph[u].append(v)
+        graph[v].append(u)
+
+    def dfs(vertex):
+        visited.add(vertex)
+        for neighbor in graph[vertex]:
+            if neighbor not in visited:
+                dfs(neighbor)
+
+    for edge in edges:
+        build_graph(edge[0], edge[1])
+
+    for vertex in range(n):
+        if vertex not in visited:
+            dfs(vertex)
+            result += 1
+    return result
+
+
 def odd_even_linked_list(head):
     if not head or not head.next:
         return head
