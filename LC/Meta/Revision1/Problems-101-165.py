28dbdaa414d1c12b2301ded237bb0326544f4a3f
boringbyte <kottapallisivaram@gmail.com>
2022-08-08 08:16:26 -0400

    Grinding leetcode finally

--- LC/Meta/Revision1/Problems-101-165.py
+++ LC/Meta/Revision1/Problems-101-165.py
@@ -131,3 +131,13 @@ def course_schedule(num_courses, prerequisites):
     return n == num_courses
 
 
+def reverse_linked_list(head):
+    prev = None
+    while head:
+        current = head
+        head = head.next
+        current.next = prev
+        prev = current
+    return prev
+
+
