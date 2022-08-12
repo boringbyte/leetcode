d70fd104b070db1e28af74fc39fd9009f2a2efaa
boringbyte <kottapallisivaram@gmail.com>
2022-08-11 21:10:44 -0400

    Grinding leetcode finally

--- LC/Meta/Revision1/Problems-01-50.py
+++ LC/Meta/Revision1/Problems-01-50.py
@@ -884,7 +884,8 @@ class LRUCache1:
         return val
 
     def put(self, key, value):
-        if key in self.cache: self.delete(key)
+        if key in self.cache:
+            self.delete(key)
         self.append(key, value)
 
 
@@ -895,13 +896,15 @@ class LRUCache2:
         self.cache = collections.OrderedDict()
 
     def get(self, key):
-        if key not in self.cache: return -1
+        if key not in self.cache:
+            return -1
         val = self.cache[key]
         self.cache.move_to_end(key)
         return val
 
     def put(self, key, val):
-        if key in self.cache: del self.cache[key]
+        if key in self.cache:
+            del self.cache[key]
         self.cache[key] = val
         if len(self.cache) > self.size:
             self.cache.popitem(last=False)
