3766afbffe5dee4544fa6414cede44e713c5263c
boringbyte <kottapallisivaram@gmail.com>
2022-09-12 22:59:23 -0400

    Grinding leetcode finally

--- LC/chewy.py
+++ LC/chewy.py
@@ -55,16 +55,17 @@ def check_anagrams2(word1, word2):
     return char_map(word1) == char_map(word2)
 
 
-def robot_exit_maze(maze, entrance):
+def robot_exit_maze(maze: list[list], entrance: list):
     # Write a program to help a robot find the exit to a maze.
     # https://leetcode.com/problems/nearest-exit-from-entrance-in-maze/
-    m, n, step, result = len(maze), len(maze[0]), 0, 0
+    m, n = len(maze), len(maze[0])
     directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-    queue = collections.deque((entrance, step))
+    queue = collections.deque([(*entrance, 0)])
+    maze[entrance[0]][entrance[1]] = '+'
 
     while queue:
         x, y, steps = queue.popleft()
-        if x in [0, m - 1] or y in [0, n - 1] and (x, y) != entrance:
+        if (x == 0 or x == m - 1 or y == 0 or y == n - 1) and [x, y] != entrance:
             return steps
         for dx, dy in directions:
             nx, ny = x + dx, y + dy
