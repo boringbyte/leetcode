9fa0fcca996313dd91ec4d37777e420184679e29
boringbyte <kottapallisivaram@gmail.com>
2025-10-12 20:07:25 -0400

    -

--- leetcode/Meta/2025/30 Days/Easy.py
+++ leetcode/Meta/2025/30 Days/Easy.py
@@ -692,17 +692,21 @@ def minimum_depth_of_binary_tree_2(root):
 def find_the_kth_character_in_string_game_i(k):
     # https://leetcode.com/problems/find-the-k-th-character-in-string-game-i/description/
     # https://leetcode.com/problems/find-the-k-th-character-in-string-game-i/solutions/6911890/beginner-freindly-java-c-python-js/
+    """
+    1. word always starts with 'a'
+    2. letter = 'abcdefghijklmnopqrstuvwxyz'
+    3. char_int_map = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7, 'i': 8, 'j': 9, 'k': 10, 'l': 11, 'm': 12, 'n': 13, 'o': 14, 'p': 15, 'q': 16, 'r': 17, 's': 18, 't': 19, 'u': 20, 'v': 21, 'w': 22, 'x': 23, 'y': 24, 'z': 25}
+    4. next_int_char_map = {1: 'b', 2: 'c', 3: 'd', 4: 'e', 5: 'f', 6: 'g', 7: 'h', 8: 'i', 9: 'j', 10: 'k', 11: 'l', 12: 'm', 13: 'n', 14: 'o', 15: 'p', 16: 'q', 17: 'r', 18: 's', 19: 't', 20: 'u', 21: 'v', 22: 'w', 23: 'x', 24: 'y', 25: 'z', 26: 'a'}
+    """
     word = ['a']
     letters = string.ascii_lowercase
-    char_int_map = {char: i for i, char in enumerate(string.ascii_lowercase)}
+    char_int_map = {char: i for i, char in enumerate(letters)}
     next_int_char_map = {i + 1: letters[(i + 1) % 26] for i in range(26)}
     while len(word) < k:
-        for i in range(len(word)):
-            # Convert character to number (0 for 'a', 1 for 'b', ..., 25 for 'z')
-            offset = char_int_map[word[i]]
-            # Get the next character cyclically (so 'z' -> 'a')
-            # next_char = chr(ord('a') + (offset + 1) % 26)
-            next_char = next_int_char_map[offset % 26]
+        # iterate on a copy to avoid modifying while looping
+        for char in list(word):
+            offset = char_int_map[char]
+            next_char = next_int_char_map[offset + 1]
             word.append(next_char)
             if len(word) >= k:
                 break
@@ -758,11 +762,12 @@ def find_missing_and_repeated_values(grid):
     return [a, b]
 
 
-def find_the_index_of_first_occurrence_in_a_string_1(haystack, needle):
+def find_the_index_of_first_occurrence_in_a_string(haystack, needle):
     # https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/solutions/665448/ac-simply-readable-python-kmp-rabin-karp/
-    # haystack.find(needle)
-
     """
+    # Using string built-in method
+    haystack.find(needle)
+
     # Robin Karp
     n, h = len(haystack), len(needle)
     needle_hash = hash(needle)
@@ -796,17 +801,20 @@ class RangeSumQueryImmutable:
 
 
 def reverse_bits_1(n):
+    """
+    bin(67) -> '0b1000011'
+    """
     binary = bin(n)[2:]
-    binary = binary.zfill(32) # binary.rjust(32, '0'), for suffix zeros ljust can be used
+    binary = binary.zfill(32)  # binary.rjust(32, '0') can also be used in place of zfill. For suffix zeros ljust can be used.
     return int(binary[::-1], 2)
 
 
 def reverse_bits_2(n):
     # https://leetcode.com/problems/reverse-bits/solutions/5228734/simple-solution-with-comments-and-explaination/
     result = 0
-    for i in range(32):
-        least_significant_bit = n & 1  # Get the rightmost bit of 'n'.
-        n = n >> 1  # Right shift to remove the least significant bit
-        result = result << 1  # Left shift to make room for the least significant bit
-        result = result | least_significant_bit  # Add the rightmost bit of 'n' to the result
+    for _ in range(32):
+        least_significant_bit = n & 1               # Get the rightmost bit of 'n'.
+        n = n >> 1                                  # Right shift to remove the least significant bit
+        result = result << 1                        # Left shift to make room for the least significant bit
+        result = result | least_significant_bit     # Add the rightmost bit of 'n' to the result
     return result
