c28a1208396c11e0996018169fd60749ad9f8f1c
boringbyte <kottapallisivaram@gmail.com>
2025-11-30 07:53:30 -0500

    three months

--- leetcode/Meta/2025/ninty_days/problems_31_40.py
+++ leetcode/Meta/2025/ninty_days/problems_31_40.py
@@ -1,4 +1,29 @@
 
 
-def merge_sorted_array():
-    pass
+def merge_sorted_array(nums1, nums2, m, n):
+    # https://leetcode.com/problems/merge-sorted-array
+
+    while m > 0 and n > 0:
+        if nums1[m - 1] >= nums2[n - 1]:
+            nums1[m + n - 1] = nums1[m - 1]
+            m - 1
+        else:
+            nums1[m + n - 1] = nums2[n - 1]
+            n - 1
+
+    if n > 0:
+        nums1[:n] = nums2[:n]
+
+
+def pascals_triangle(num_rows):
+    # https://leetcode.com/problems/pascals-triangle/description/
+    row, result = [1], [[1]]
+
+    for _ in range(1, num_rows):
+        left = [0] + row
+        right = row + [0]
+        row = [x + y for x, y in zip(left, right)]
+        result.append(row)
+    return result
+
+
